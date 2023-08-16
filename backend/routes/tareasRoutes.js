ce52adacce7786e8bed341f6093d6f84041a7d77
Marco Garcia <marcogarcia.gon@gmail.com>
2023-08-15 18:05:10 -0600

    fix: URI porotected

--- backend/routes/tareasRoutes.js
+++ backend/routes/tareasRoutes.js
@@ -3,20 +3,25 @@ const router = express.Router()
 const {getTareas,  createTareas, updateTareas, deleteTareas} = require('../controllers/tareaControllers')
 const {protect} = require('../middleware/authMiddleware')
 
-router.get('/', getTareas)
+// Protegemos la ruta con el protect del middleware
+router.route('/').get(protect, getTareas).post(protect, createTareas)
     // Aqui mando la respuesta plain, pero podemos controlar que solo cuando todo esté OK con un status
     // res.send({"message": "Get tareas"})
 
     // Aqui configurmaos que sea cuando toso esté OK con el status
     // res.status(200).json({"message": "Get tareas"})
 
-router.post('/', createTareas)
+// En la siguiente linea cualquier puede consultar las tareas con una URL válida, pero arriba la protegemos
+// router.post('/', createTareas)
 
 // Podríamos simplificar así las siguientes lineas en una sola:
 // router.route('/:id').delete(deleteTareas).put(updateTareas)
 
-router.put('/:id', updateTareas)
+// En la siguiente linea cualquier puede consultar las tareas con una URL válida, pero arriba la protegemos
+// router.put('/:id', updateTareas)
+// router.delete('/:id', deleteTareas)
 
-router.delete('/:id', deleteTareas)
+// Protegemos la ruta con el protect del middleware
+router.route('/:id').delete(protect, deleteTareas).put(protect, deleteTareas)
 
 module.exports = router
